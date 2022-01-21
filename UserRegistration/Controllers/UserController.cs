e0428735798b8984d22533ae53d1273b3ebeaede
v-saigattu <v-saigattu@microsoft.com>
2022-01-21 11:03:12 +0530

    Initial repo checkin

---
+++ UserRegistration/Controllers/UserController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel;
+using UserRegistration.Models;
+using System.Web.Security;
+
+
+namespace UserRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+       
+        [HttpGet]
+        public ActionResult AddOrEdit(int id=0)
+        {
+            User userModel = new User();
+
+            return View(userModel);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(User UserModel)
+        {
+            using (DbModels dbModel=new DbModels())
+            {
+                if(dbModel.Users.Any(x=>x.Username==UserModel.Username))
+                {
+                    ViewBag.DuplicateMessage = "Username already exists";
+                    return View("AddOrEdit", UserModel);
+                }
+                dbModel.Users.Add(UserModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+            return View("AddOrEdit",new User());
+        }
+
+
+
+        [HttpGet]
+        public ActionResult LogOn()
+        {
+         return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOn(User UserModel)
+        {
+            using (DbModels db = new DbModels())
+            {
+                var usr = db.Users.Single(u => u.Username == UserModel.Username && u.Password == UserModel.Password);
+                if (usr != null)
+                {
+                    Session["UserId"] = usr.UserID.ToString();
+                    Session["Username"] = usr.Username.ToString();
+                    return RedirectToAction("LoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or Password is wrong.");
+                }
+
+            }
+            return View();
+        }
+        public ActionResult LoggedIn()
+        {
+            if(Session["UserId"]!=null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("LogOn");
+            }
+           
+        }
+
+
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("LogOn", "User");
+        }
+
+
+
+
+    }
+}
---
+++ UserRegistration/Models/LogOn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UserRegistration.Models
+{
+    public class LogOn
+    {
+
+
+        [DisplayName("Username")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Username required.")]
+        public string Username { get; set; }
+
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+
+        [DisplayName("Remember Me")]
+        public bool RememberMe { get; set; }
+
+
+
+
+    }
+
+}
